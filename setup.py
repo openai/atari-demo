99faff3e3289b4299454ecfbb698f38f77c402a1
Tim Salimans <>
2018-06-27 16:14:03 +0200

    remove unused

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='atari-demo',
-      py_modules=['atari_demo'],
-      install_requires=['numpy'],
-      description="Tool for collecting demonstrations for Atari",
-      author="Tim Salimans",
-)
